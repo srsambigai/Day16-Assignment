4f12518a8706b3b61e83bc5927a0d7e7923cc96f
Swarna Senthil <srsambigai@gmail.com>
2023-12-10 20:37:33 -0600

    Callback Hell

---
+++ script.js
@@ -0,0 +1,41 @@
+
+setTimeout(()=>{
+    document.getElementById("divid").innerHTML = "<h2> 10 </h2>";
+   // document.getElementById("divid").style.padding = "70px 0";
+   // document.getElementById("divid").style.border="3px solid green";
+   document.getElementById("divid").style.display="flex";
+    document.getElementById("divid").style.justifyContent="center";
+    document.getElementById("divid").style.alignItems="center";
+    setTimeout(()=>{
+        document.getElementById("divid").innerHTML = "<h2> 9 </h2>";
+        setTimeout(()=>{
+            document.getElementById("divid").innerHTML = "<h2> 8 </h2>";
+            setTimeout(()=>{
+                document.getElementById("divid").innerHTML = "<h2> 7 </h2>";
+                setTimeout(()=>{
+                    document.getElementById("divid").innerHTML = "<h2> 6 </h2>";
+                    setTimeout(()=>{
+                        document.getElementById("divid").innerHTML = "<h2> 5 </h2>";
+                        setTimeout(()=>{
+                            document.getElementById("divid").innerHTML = "<h2> 4 </h2>";
+                            setTimeout(()=>{
+                                document.getElementById("divid").innerHTML = "<h2> 3 </h2>";
+                                setTimeout(()=>{
+                                    document.getElementById("divid").innerHTML = "<h2> 2 </h2>";
+                                    setTimeout(()=>{
+                                        document.getElementById("divid").innerHTML = "<h2> 1 </h2>";
+                                        setTimeout(()=>{
+                                            document.getElementById("divid").innerHTML = "<h1> <b>Happy Independence Day !!!</b> </h1>";
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+
+
